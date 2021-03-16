fb51d166b831ca955f9e0dd924cb03311559bfe8
tongwai <emlynwong@gmail.com>
2021-03-16 09:39:15 +0800

    add #326

---
+++ 326powerOfThree.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
+        if n == 1:
+            return True
+
+        while n > 1:
+            q = n // 3
+            r = n % 3
+            if r != 0:
+                return False
+            elif q == 1 :
+                return True
+            n = q

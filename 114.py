bf90b8d1446e7933e860dce2ec7c433d9f306ddc
tongwai <emlynwong@gmail.com>
2021-04-05 10:18:44 +0800

    add #114

---
+++ 114.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        exist = {}
+        while head:
+            if head in exist:
+                return True
+            exist[head] = True
+            head = head.next
+        return False
+
+
+class Solution2:
+    def hasCycle(self, head: ListNode) -> bool:
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
